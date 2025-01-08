d5bea3fbed41af26ca13940f50dfd3238c1f4e32
Byron Hernandez <145072285+rokoisloco@users.noreply.github.com>
2025-01-08 15:26:15 -0800

    Part 1 A*
    ok

--- WayPointsAndGraphsHernandezP7/Assets/Scripts/FollowWP.cs
+++ WayPointsAndGraphsHernandezP7/Assets/Scripts/FollowWP.cs
@@ -9,27 +9,43 @@ public class FollowWP : MonoBehaviour
 
     public float speed = 10.0f;
     public float rotspeed = 10.0f;
+    public float lookAhead = 10.0f;
+
+    GameObject tracker;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        tracker = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        DestroyImmediate(tracker.GetComponent<Collider>());
+        tracker.GetComponent<MeshRenderer>().enabled = false;
+        tracker.transform.position = this.transform.position;
+        tracker.transform.rotation = this.transform.rotation;
     }
 
-    // Update is called once per frame
-    void Update()
+    void ProgressTracker()
     {
-        if (Vector3.Distance(this.transform.position, waypoints[currentWP].transform.position) < 3)
+
+        if (Vector3.Distance(tracker.transform.position, this.transform.position) > lookAhead) return;
+
+        if (Vector3.Distance(tracker.transform.position, waypoints[currentWP].transform.position) < 3)
             currentWP++;
 
-        if ( currentWP >= waypoints.Length )
+        if (currentWP >= waypoints.Length)
             currentWP = 0;
+
+        tracker.transform.LookAt(waypoints[currentWP].transform);
+        tracker.transform.Translate(0, 0, (speed + 2) * Time.deltaTime);
+    }
+    void Update()
+    {
+       ProgressTracker();
 
   
 
         Quaternion lookatWP = Quaternion.LookRotation(waypoints[currentWP].transform.position - this.transform.position);
-        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookatWP, rotspeed * Time.deltaTime);   
+       this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookatWP, rotspeed * Time.deltaTime);   
         this.transform.Translate(0, 0, speed * Time.deltaTime );
     }
 }
